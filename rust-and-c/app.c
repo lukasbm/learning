d2660e9db0049345a0bee1048415848cf69362e2
Lukas <lukasbm@users.noreply.github.com>
2023-02-08 17:29:35 +0100

    :poop: broken linker

--- rust-and-c/app.c
+++ rust-and-c/app.c
@@ -1,1 +1,10 @@
-int main() { return 0; }
+#include "fibonacci.h"
+#include <stdio.h>
+
+int main() {
+  for (int32_t i = 0; i < 10; i++) {
+    uint64_t f = fibonacci(i);
+    printf("fib %i: %li", i, f);
+  }
+  return 0;
+}
