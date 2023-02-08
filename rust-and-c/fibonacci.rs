94ce3e1e2aa23fed27aa40e7bb59cf7f6d16c096
Lukas <lukasbm@users.noreply.github.com>
2023-02-08 22:17:28 +0100

    :poop:

--- rust-and-c/fibonacci.rs
+++ rust-and-c/fibonacci.rs
@@ -1,6 +1,7 @@
-#![no_main]
+// #![feature(lang_items)]
 #![no_std]
-
+#![no_main]
+// use core::intrinsics;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -18,3 +19,11 @@ pub extern "C" fn fibonacci(n: i32) -> u64 {
         _ => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
+
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+// #[lang = "eh_personality"] extern fn rust_eh_personality() {}
+// #[lang = "panic_impl"] extern fn rust_begin_panic(info: &PanicInfo) -> ! { unsafe { intrinsics::abort() } }
+// #[no_mangle] pub extern fn rust_eh_register_frames () {}
+// #[no_mangle] pub extern fn rust_eh_unregister_frames () {}
