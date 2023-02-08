a14ddb65693508eb8f5ebab7f4a3646ecf20ab4e
Lukas <lukasbm@users.noreply.github.com>
2023-02-08 17:15:33 +0100

    :bug: fix cargo

--- rust-and-c/fibonacci.rs
+++ rust-and-c/fibonacci.rs
@@ -1,1 +1,10 @@
-fn fibonacci(n: int) {}
+#[no_mangle]
+pub extern "C" fn fibonacci(n: i32) -> u64 {
+    if n <= 0 {
+        panic!("no negative arguments");
+    }
+    match n {
+        1 | 2 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
