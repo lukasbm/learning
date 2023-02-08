01a81cd3c1e87097050480586aaa110688dc1755
Lukas <lukasbm@users.noreply.github.com>
2023-02-08 22:03:33 +0100

    :construction: attempt to rewrite into no std lib

--- rust-and-c/fibonacci.rs
+++ rust-and-c/fibonacci.rs
@@ -1,3 +1,13 @@
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
 #[no_mangle]
 pub extern "C" fn fibonacci(n: i32) -> u64 {
     if n <= 0 {
